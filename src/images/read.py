a3c3f8eab9868b63a334e1913942bf94fdaaf57e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-14 00:32:48 +0100

    Add src/images/read.py

---
+++ src/images/read.py
@@ -0,0 +1,29 @@
+import src.functions.dearchive
+import os
+import dask
+
+
+class Read:
+    
+    def __init__(self):
+        """
+        
+        """
+        
+        self.__dearchive = src.functions.dearchive.Dearchive(path=os.path.join(os.getcwd(), 'images'))
+
+    @dask.delayed
+    def __executing(self, url) -> str:
+        
+        return self.__dearchive.exc(url=url)
+        
+    def exc(self, strings: list[str]) -> list:
+
+        # Get Images
+        computations = []
+        for string in strings[:12]:
+            message = self.__executing(url=string)
+            computations.append(message)
+        messages = dask.compute(computations, scheduler='threads')[0]
+
+        return messages
