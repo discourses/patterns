f459d3ab18110e4fc724c1e425bce495927bef03
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-13 23:32:44 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import os
 import logging
 import sys
 
+import numpy as np
+
 import tensorflow as tf
 
 
@@ -17,6 +19,11 @@ def main():
     devices = tf.config.list_physical_devices('GPU')
     logger.info(devices)
 
+    # An experiment
+    endpoint = 'https://github.com/greyhypotheses/dermatology/raw/master/augmentations/images/{name}.zip'
+    strings = [endpoint.format(name=str(number).zfill(3)) for number in np.arange(0, 16)]
+    logger.info(strings)
+
 
 if __name__ == '__main__':
 
@@ -30,5 +37,8 @@ if __name__ == '__main__':
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(__name__)
+
+    # Classes
+    import src.functions.dearchive
 
     main()
