45324dea1551c149e3b0850c38bb042e45f877cf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-13 22:55:39 +0100

    Update src/functions/Archives.py

--- src/functions/Archives.py
+++ src/functions/Archives.py
@@ -1,3 +1,8 @@
+import requests
+import zipfile
+import io
+import os
+import sys
 
 
 class Archives:
@@ -6,3 +11,25 @@ class Archives:
         """
 
         """
+
+    def __directories(self):
+        """
+
+        :return:
+        """
+
+    def exc(self, url: str, path: str):
+        """
+
+        :param url:
+        :param path:
+        :return:
+        """
+
+        try:
+            request = requests.get(url=url)
+        except requests.HTTPError as err:
+            raise Exception(err)
+
+        objects = zipfile.ZipFile(io.BytesIO(request.content))
+        objects.extractall(path=path)
