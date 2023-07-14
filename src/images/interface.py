b3648fda30870ac6f753802ed5833946aa997fe7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-14 13:48:34 +0100

    Update src/images/interface.py

--- src/images/interface.py
+++ src/images/interface.py
@@ -1,42 +1,40 @@
+"""
+interface.py
+"""
 import logging
-import os
-import sys
 
 import numpy as np
 
+import src.images.read
 
-def main():
+
+class Interface:
+    """
+    Class Interface
     """
 
-    :return:
-    """
+    def __init__(self):
+        """
+        Constructor
+        """
 
-    # The URL strings of the images zip files
-    endpoint = 'https://github.com/greyhypotheses/dermatology/raw/master/augmentations/images/{name}.zip'
-    strings = [endpoint.format(name=str(number).zfill(3)) for number in np.arange(0, 196)]
-    logger.info(strings)
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
 
-    messages = src.images.read.Read().exc(strings=strings)
-    logger.info(messages)
+        # The URL strings of the images zip files
+        self.__endpoint = 'https://github.com/greyhypotheses/dermatology/raw/master/augmentations/images/{name}.zip'
 
+    def exc(self):
+        """
 
-if __name__ == '__main__':
+        :return:
+        """
 
-    # Paths
-    root = os.getcwd()
-    sys.path.append(root)
-    sys.path.append(os.path.join(root, 'src'))
+        strings = [self.__endpoint.format(name=str(number).zfill(3)) for number in np.arange(0, 196)]
+        self.__logger.info(strings)
 
-    # Threads
-    os.environ['NUMEXPR_MAX_THREADS'] = '12'
-
-    # Logging
-    logging.basicConfig(level=logging.INFO,
-                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    logger = logging.getLogger(__name__)
-
-    # Classes
-    import src.images.read
-
-    main()
+        messages = src.images.read.Read().exc(strings=strings)
+        self.__logger.info(messages)
