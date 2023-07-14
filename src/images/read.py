5e9194e58a0244ee2df2d26d6a0345fe6c7c3cba
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-14 13:48:49 +0100

    Update src/images/read.py

--- src/images/read.py
+++ src/images/read.py
@@ -1,23 +1,41 @@
-import src.functions.dearchive
+"""
+read.py
+"""
 import os
+
 import dask
+
+import src.functions.dearchive
 
 
 class Read:
-    
+    """
+    Class Read
+    """
+
     def __init__(self):
         """
-        
+        Constructor
         """
-        
+
         self.__dearchive = src.functions.dearchive.Dearchive(path=os.path.join(os.getcwd(), 'images'))
 
     @dask.delayed
     def __executing(self, url) -> str:
-        
+        """
+
+        :param url:
+        :return:
+        """
+
         return self.__dearchive.external(url=url)
-        
+
     def exc(self, strings: list[str]) -> list:
+        """
+
+        :param strings:
+        :return:
+        """
 
         # Get Images
         computations = []
