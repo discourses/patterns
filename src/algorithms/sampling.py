c8936bec425840c42c8e469199211339a626513b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-15 08:37:43 +0100

    Add src/algorithms/sampling.py

---
+++ src/algorithms/sampling.py
@@ -0,0 +1,20 @@
+"""
+sampling.py
+"""
+
+
+class Sampling:
+    """
+    Class Sampling
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+    def exc(self):
+        """
+
+        :return:
+        """
