f2bf0b5f20cdf70bd14a6323a4d9653bf144dde6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-15 13:11:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config:
     Class Config
     """
 
-    Modelling = collections.namedtuple(
-        typename='Modelling',
+    Settings = collections.namedtuple(
+        typename='Settings',
         field_names=['sample', 'replace', 'class_sample_size', 'epochs', 'features', 'random_state',
                      'minimum_class_instances', 'batch_size', 'train_size_initial', 'train_size_evaluation',
                      'early_stopping_patience', 'model_checkpoints_directory', 'error_matrix_variables'])
