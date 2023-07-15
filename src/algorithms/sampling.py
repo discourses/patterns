cbb07396263e86c17eb37e5a820d76e2338a6548
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-15 12:32:13 +0100

    Update src/algorithms/sampling.py

--- src/algorithms/sampling.py
+++ src/algorithms/sampling.py
@@ -16,12 +16,12 @@ class Sampling:
     by comparing the -number of records per label- & -sample size requested-
     """
 
-    def __init__(self, attributes: config.Config().Modelling, metadata: config.Config().Metadata):
+    def __init__(self, settings: config.Config().Modelling, metadata: config.Config().Metadata):
         """
         Constructor
         """
 
-        self.__attributes = attributes
+        self.__settings = settings
         self.__metadata = metadata
 
         # Logging
@@ -39,35 +39,35 @@ class Sampling:
         :return:
         """
 
-        if (not self.__attributes.replace) & (n_per_label.min() < self.__attributes.class_sample_size):
+        if (not self.__settings.replace) & (n_per_label.min() < self.__settings.class_sample_size):
             class_sample_size = n_per_label.min()
             self.__logger.info(f'Case: Sampling with replacement.\n\nNote, the sample size per class '
                                f'will be {class_sample_size}, i.e., the '
                                'sample size of the smallest class because it has fewer images than the requested '
-                               f'sample size per class; {self.__attributes.class_sample_size}.  Altogether, this '
+                               f'sample size per class; {self.__settings.class_sample_size}.  Altogether, this '
                                f'ensures a balanced data set.')
         else:
-            class_sample_size = self.__attributes.class_sample_size
+            class_sample_size = self.__settings.class_sample_size
 
         return class_sample_size
 
-    def exc(self, data: pd.DataFrame) -> pd.DataFrame:
+    def exc(self, register: pd.DataFrame) -> pd.DataFrame:
         """
 
-        :param data: The images register
+        :param register: The images register
         :return:
             A sample of the register
         """
 
         # __sample_size()
-        n_per_label: pd.Series = data[self.__metadata.labels].sum(axis=0)
+        n_per_label: pd.Series = register[self.__metadata.labels].sum(axis=0)
         class_sample_size: int = self.__sample_size(n_per_label)
 
         # Hence
-        self.__logger.info(data.groupby(self.__metadata.labels)[self.__metadata.fields + self.__metadata.labels])
-        excerpt = data.groupby(self.__metadata.labels)[self.__metadata.fields + self.__metadata.labels] \
-            .apply(lambda x: x.sample(n=class_sample_size, replace=self.__attributes.replace,
-                                      random_state=self.__attributes.random_state))
+        self.__logger.info(register.groupby(self.__metadata.labels)[self.__metadata.fields + self.__metadata.labels])
+        excerpt = register.groupby(self.__metadata.labels)[self.__metadata.fields + self.__metadata.labels] \
+            .apply(lambda x: x.sample(n=class_sample_size, replace=self.__settings.replace,
+                                      random_state=self.__settings.random_state))
         excerpt.reset_index(drop=True, inplace=True)
 
         return excerpt
