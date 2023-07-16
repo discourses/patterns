fe6f593d42d4593d5b8924a712a4639cedf399fd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-16 14:24:39 +0100

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,10 @@ class Config:
     Class Config
     """
 
+    '''
+    The values of Settings, Metadata, & Source are defined in 
+    descriptors/images.yml
+    '''
     Settings = collections.namedtuple(
         typename='Settings',
         field_names=['sample', 'replace', 'class_sample_size', 'epochs', 'random_state',
@@ -16,12 +20,17 @@ class Config:
                      'early_stopping_patience', 'model_checkpoints_directory', 'error_matrix_variables'])
 
     Metadata = collections.namedtuple(
-        typename='Metadata',
-        field_names=['url', 'key', 'fields', 'labels'])
+        typename='Metadata', field_names=['url', 'key', 'fields', 'labels'])
 
     Source = collections.namedtuple(
-        typename='Source',
-        field_names=['url', 'index_from', 'index_to', 'index_zero_filling', 'ext', 'directory'])
+        typename='Source', field_names=['url', 'index_from', 'index_to', 'index_zero_filling', 'ext', 'directory'])
+
+    '''
+    These are the metadata data frames of a prospective set of 
+    training, validating, & testing data
+    '''
+    Partitions = collections.namedtuple(
+        typename='Partitions', field_names=['training', 'validating', 'testing'])
 
     def __init__(self):
         """
