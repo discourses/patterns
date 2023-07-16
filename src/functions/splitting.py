424b93260e3ae7f2dc947c3c04769f8e0a21a49c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-16 13:53:47 +0100

    Update src/functions/splitting.py

--- src/functions/splitting.py
+++ src/functions/splitting.py
@@ -10,25 +10,49 @@ class Splitting:
     Class Splitting
     """
 
-    def __init__(self):
+    def __init__(self, random_state: int):
         """
         Constructor
         """
 
-    @staticmethod
-    def exc(independent: pd.DataFrame, dependent: pd.DataFrame,
-            train_size: float, random_state: int, stratify=None) -> (pd.DataFrame, pd.DataFrame,
-                                                                     pd.DataFrame, pd.DataFrame):
+        self.__random_state = random_state
+
+    def __splitting(self, independent: pd.DataFrame, dependent: pd.DataFrame,
+                    train_size: float, stratify=None) -> (pd.DataFrame, pd.DataFrame):
+        """
+
+        :param independent:
+        :param dependent:
+        :param train_size:
+        :param stratify:
+        :return:
+        """
+
+        x_training: pd.DataFrame
+        x_testing: pd.DataFrame
+        y_training: pd.DataFrame
+        y_testing: pd.DataFrame
+        x_training, x_testing, y_training, y_testing = sms.train_test_split(
+            independent, dependent, train_size=train_size, random_state=self.__random_state, stratify=stratify)
+
+        training = x_training.join(y_training)
+        testing = x_testing.join(y_testing)
+
+        return training, testing
+
+    def exc(self, independent: pd.DataFrame, dependent: pd.DataFrame,
+            train_size: float, stratify=None) -> (pd.DataFrame, pd.DataFrame):
         """
         Stratified splitting of a dataset
 
         :param independent:
         :param dependent:
         :param train_size:
-        :param random_state:
         :param stratify:
         :return:
+            training: pandas.DataFrame
+            testing: pandas.DataFrame
         """
 
-        return sms.train_test_split(independent, dependent, train_size=train_size,
-                                    random_state=random_state, stratify=stratify)
+        return self.__splitting(independent=independent, dependent=dependent,
+                                train_size=train_size, stratify=stratify)
