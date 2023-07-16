fb45dacdc02319fd6ad23e37d50124e13809c7e9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-16 14:25:11 +0100

    Update src/modelling/splits.py

--- src/modelling/splits.py
+++ src/modelling/splits.py
@@ -17,6 +17,7 @@ class Splits:
 
     Settings = config.Config().Settings
     Metadata = config.Config().Metadata
+    Partitions = config.Config().Partitions
 
     def __init__(self, settings: Settings, metadata: Metadata):
         """
@@ -32,24 +33,29 @@ class Splits:
         # Instances
         self.__splitting = src.functions.splitting.Splitting(random_state=self.__settings.random_state)
         
-    def __splits(self, data: pd.DataFrame, train_size: float):
+    def __splits(self, data: pd.DataFrame, train_size: float) -> (pd.DataFrame, pd.DataFrame):
+        """
+
+        :param data:
+        :param train_size:
+        :return:
+        """
 
         return self.__splitting.exc(
             independent=data['path'], dependent=data[self.__metadata.labels],
             train_size=train_size, stratify=data[self.__metadata.labels])
 
-    def exc(self, sample: pd.DataFrame):
+    def exc(self, sample: pd.DataFrame) -> Partitions:
         """
 
+        :param sample:
         :return:
         """
 
-        training: pd.DataFrame
-        evaluating: pd.DataFrame
         training, evaluating = self.__splits(
             data=sample, train_size=self.__settings.train_size_initial)
 
-        validating: pd.DataFrame
-        testing: pd.DataFrame
         validating, testing = self.__splits(
             data=evaluating, train_size=self.__settings.train_size_evaluation)
+
+        self.Partitions(training=training, validating=validating, testing=testing)
