f1715ff2efa05e34935b36d423565a6a3b66df96
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-16 11:00:59 +0100

    Update src/modelling/splits.py

---
+++ src/modelling/splits.py
@@ -0,0 +1,22 @@
+"""
+splits.py
+"""
+
+
+class Splits:
+    """
+    Class Splits
+
+    Delivers the learning, validation, and testing splits
+    """
+
+    def __init__(self):
+        """
+
+        """
+
+    def exc(self):
+        """
+
+        :return:
+        """
