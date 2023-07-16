00a2adfb84623ece557b08fb19db08c737e0ecb4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-16 13:54:34 +0100

    Update src/modelling/splits.py

--- src/modelling/splits.py
+++ src/modelling/splits.py
@@ -1,6 +1,11 @@
 """
 splits.py
 """
+import pandas as pd
+
+import src.functions.splitting
+
+import config
 
 
 class Splits:
@@ -10,13 +15,41 @@ class Splits:
     Delivers the learning, validation, and testing splits
     """
 
-    def __init__(self):
+    Settings = config.Config().Settings
+    Metadata = config.Config().Metadata
+
+    def __init__(self, settings: Settings, metadata: Metadata):
         """
 
+        :param settings:
+        :param metadata:
         """
 
-    def exc(self):
+        # Data Classes
+        self.__settings = settings
+        self.__metadata = metadata
+
+        # Instances
+        self.__splitting = src.functions.splitting.Splitting(random_state=self.__settings.random_state)
+        
+    def __splits(self, data: pd.DataFrame, train_size: float):
+
+        return self.__splitting.exc(
+            independent=data['path'], dependent=data[self.__metadata.labels],
+            train_size=train_size, stratify=data[self.__metadata.labels])
+
+    def exc(self, sample: pd.DataFrame):
         """
 
         :return:
         """
+
+        training: pd.DataFrame
+        evaluating: pd.DataFrame
+        training, evaluating = self.__splits(
+            data=sample, train_size=self.__settings.train_size_initial)
+
+        validating: pd.DataFrame
+        testing: pd.DataFrame
+        validating, testing = self.__splits(
+            data=evaluating, train_size=self.__settings.train_size_evaluation)
