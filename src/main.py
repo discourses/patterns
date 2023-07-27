d050ea181f45d2250c59182257424776b99a5a15
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-27 23:06:34 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import logging
 import os
 import sys
 import tensorflow as tf
+import tensorrt as trt
 
 
 def main():
@@ -15,6 +16,9 @@ def main():
     """
 
     logger.info('Patterns')
+
+    # Inspecting
+    logger.info(trt.__version__)
 
     # Devices; the machine has two GPU (graphics processing units) devices
     tf.debugging.set_log_device_placement(True)
