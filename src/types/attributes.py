5001964c5e287fe9c53403bf3222967668ac12e6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-26 19:28:11 +0100

    Update src/types/attributes.py

---
+++ src/types/attributes.py
@@ -0,0 +1,21 @@
+"""
+This is data type Attributes
+"""
+import os
+import typing
+import src.algorithms.descriptors
+
+
+class Attributes(typing.NamedTuple):
+    """
+    The attributes class
+    """
+
+    dictionary = src.algorithms.descriptors.Descriptors(
+        path=os.path.join(os.getcwd(), 'data', 'images.yml')).exc(node=['data', 'attributes'])
+
+    ext: str = dictionary['ext']
+    rows: int = dictionary['rows']
+    columns: int = dictionary['columns']
+    channels: int = dictionary['channels']
+    rotations: list[int] = dictionary['rotations']
