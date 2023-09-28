e98ca2b0d1f7616a8c9cc78d2481343f7b306794
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-28 06:48:55 +0100

    Update src/modelling/interface.py

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -54,7 +54,7 @@ class Interface:
 
         partitions = src.modelling.splits.Splits(
             settings=self.__settings, metadata=self.__metadata).exc(sample=sample)
-        self.__logger.info('Training %s, Validating %s, Testing %s', 
+        self.__logger.info('Training %s, Validating %s, Testing %s',
                            partitions.training.shape, partitions.validating.shape, partitions.testing.shape)
 
         training = self.__pipeline.exc(data=partitions.training, testing=False)
