18d21f802049c155ed01b9f3b104639ce9413c49
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-28 19:30:39 +0100

    Update src/modelling/architecture.py

--- src/modelling/architecture.py
+++ src/modelling/architecture.py
@@ -4,6 +4,7 @@ The architecture of the deep learning model
 import tensorflow as tf
 
 import src.elements.attributes
+import src.elements.hpc
 
 
 class Architecture:
@@ -36,7 +37,7 @@ class Architecture:
 
         return base
 
-    def exc(self):
+    def exc(self, hpc: src.elements.hpc.HPC, labels: list, metrics: list = None):
         """
         
         :return:
@@ -45,4 +46,34 @@ class Architecture:
         # The base#
         base = self.__baseline()
 
-        print(base)
+        # Flattening Object
+        flatten = tf.keras.layers.Flatten()
+
+        # The fully connected layers
+        alpha_units = tf.keras.layers.Dense(hpc.alpha_units, activation='relu', name='Alpha')
+        alpha_dropout = tf.keras.layers.Dropout(rate=hpc.alpha_dropout, name='AlphaDropout')
+        beta_units = tf.keras.layers.Dense(hpc.beta_units, activation='relu', name='Beta')
+        beta_dropout = tf.keras.layers.Dropout(hpc.beta_dropout, name='BetaDropout')
+
+        # The classification layer
+        classifier = tf.keras.layers.Dense(len(labels), activation=tf.nn.softmax)
+
+        # Build
+        model = tf.keras.models.Sequential([base, flatten, alpha_units, alpha_dropout,
+                                            beta_units, beta_dropout, classifier])
+        
+        # Labels. Case
+        # one-hot-code: categorical_crossentropy
+        # integers: sparse_categorical_crossentropy
+        if metrics is None:
+            model.compile(optimizer=hpc.opt,
+                          loss=tf.keras.losses.categorical_crossentropy)
+        else:
+            model.compile(optimizer=hpc.opt,
+                          metrics=metrics,
+                          loss=tf.keras.losses.categorical_crossentropy)
+
+        print(base.summary())
+        print(model.summary())
+
+        return model
