884efe9e60c726fa2a30dcd739236d1a95fc1939
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-29 19:54:59 +0100

    Update src/modelling/estimating.py: In Progress

--- src/modelling/estimating.py
+++ src/modelling/estimating.py
@@ -1,8 +1,14 @@
 """
 Estimating a model's parameters
 """
+import math
+
+import tensorflow as tf
 
 import src.elements.settings
+import src.elements.generators
+import src.elements.partitions
+import src.evaluation.endpoints
 
 
 class Estimating:
@@ -10,20 +16,49 @@ class Estimating:
     Class Estimating
     """
 
-    def __init__(self, settings: src.elements.settings.Settings) -> None:
+    def __init__(self, settings: src.elements.settings.Settings, 
+                 generators: src.elements.generators.Generators, 
+                 partitions: src.elements.partitions.Partitions) -> None:
         """
         
         :param settings:
         """
 
-        self.__batch_size = settings.batch_size
-        self.__early_stopping_patience = settings.early_stopping_patience
+        self.__settings = settings
+        self.__generators = generators
+        self.__partitions = partitions
 
-    def network(self):
+
+    def __endpoints(self):
+
+        endpoints = src.evaluation.endpoints.Endpoints(settings=self.__settings)
+
+        early_stopping = endpoints.early_stopping()
+        model_checkpoint = endpoints.model_checkpoint(network_checkpoints_path='')
+
+        return early_stopping, model_checkpoint
+
+    def __estimate(self, model: tf.keras.Sequential):
+
+        early_stopping, model_checkpoint = self.__endpoints()
+
+        # Steps per epoch
+        steps_per_epoch = math.ceil(self.__partitions.training.shape[0] / self.__settings.batch_size)
+
+        # Validation steps
+        validation_steps = math.ceil(self.__partitions.validating.shape[0] / self.__settings.batch_size)
+
+        history = model.fit_generator(
+            generator=self.__generators.training, steps_per_epoch=steps_per_epoch, epochs=self.__settings.epochs,
+            verbose=1, callbacks=[early_stopping, model_checkpoint], validation_data=self.__generators.validating,
+            validation_steps=validation_steps, validation_freq=1)
+
+        return history
+
+    def network(self, model: tf.keras.Sequential):
         """
         
         :return:
         """
 
-        print(self.__batch_size)
-        print(self.__early_stopping_patience)
+        self.__estimate(model=model)
