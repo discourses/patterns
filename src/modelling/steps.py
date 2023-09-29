1e7decf8fb6926086e7160c253355aae4e4f4eeb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-29 20:24:01 +0100

    Update src/modelling/steps.py

--- src/modelling/steps.py
+++ src/modelling/steps.py
@@ -4,7 +4,9 @@ The modelling steps for interface.py
 import src.modelling.hyperparameters
 import src.modelling.architecture
 import src.elements.attributes
+import src.elements.generators
 import src.elements.metadata
+import src.elements.partitions
 
 class Steps:
     """
@@ -20,7 +22,7 @@ class Steps:
         self.__architecture = src.modelling.architecture.Architecture(attributes=attributes)
         self.__metadata = metadata
 
-    def exc(self):
+    def exc(self, generators: src.elements.generators.Generators, partitions: src.elements.partitions.Partitions):
         """
         
         :return:
@@ -35,3 +37,5 @@ class Steps:
 
             model = self.__architecture.exc(hpc=hpc, labels=self.__metadata.labels)
             print(type(model))
+            print(generators._fields)
+            print(partitions._fields)
