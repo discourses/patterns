af94970cdc4fd22a0545496e02f3a2e371fd6ff2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-29 19:55:20 +0100

    Update src/modelling/steps.py: In Progress

--- src/modelling/steps.py
+++ src/modelling/steps.py
@@ -2,18 +2,23 @@
 The modelling steps for interface.py
 """
 import src.modelling.hyperparameters
+import src.modelling.architecture
+import src.elements.attributes
+import src.elements.metadata
 
 class Steps:
     """
     Class Steps
     """
 
-    def __init__(self) -> None:
+    def __init__(self, attributes: src.elements.attributes.Attributes, metadata: src.elements.metadata.Metadata) -> None:
         """
         Constructor
         """
 
         self.__hyperparameters = src.modelling.hyperparameters.Hyperparameters().exc()
+        self.__architecture = src.modelling.architecture.Architecture(attributes=attributes)
+        self.__metadata = metadata
 
     def exc(self):
         """
@@ -28,4 +33,5 @@ class Steps:
             index = index + 1
             str(index).zfill(4)
 
-            hpc
+            model = self.__architecture.exc(hpc=hpc, labels=self.__metadata.labels)
+            print(type(model))
