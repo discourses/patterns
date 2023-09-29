fa8d80dc2f58e3247454e55cff8ae154f995f3c4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-29 15:47:51 +0100

    Update src/modelling/estimating.py: In Progress

--- src/modelling/estimating.py
+++ src/modelling/estimating.py
@@ -2,17 +2,28 @@
 Estimating a model's parameters
 """
 
+import src.elements.settings
+
 
 class Estimating:
     """
     Class Estimating
     """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, settings: src.elements.settings.Settings) -> None:
+        """
+        
+        :param settings:
+        """
+
+        self.__batch_size = settings.batch_size
+        self.__early_stopping_patience = settings.early_stopping_patience
 
     def network(self):
         """
         
-        :return;
+        :return:
         """
+
+        print(self.__batch_size)
+        print(self.__early_stopping_patience)
