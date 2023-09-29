7eec4428c6b84d47937c9b690cd744b0ab9b1851
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-29 18:15:46 +0100

    Add src/evaluation/endpoints.py

---
+++ src/evaluation/endpoints.py
@@ -0,0 +1,46 @@
+"""
+Model points
+"""
+
+import os
+
+import tensorflow as tf
+
+import src.elements.settings
+
+
+class Endpoints:
+    """
+    Class Endpoints
+    """
+
+    def __init__(self, settings: src.elements.settings.Settings) -> None:
+        """
+        
+        :param settings:
+        """
+
+        self.__settings = settings
+
+    def early_stopping(self):
+        """
+        
+        :return:
+        """
+
+        return tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss', verbose=1, patience=self.__settings.early_stopping_patience, 
+            mode='min', restore_best_weights=True)
+
+    @staticmethod
+    def model_checkpoint(network_checkpoints_path: str):
+        """
+        
+        :param network_checkpoints_path:
+        :return:
+        """
+
+        return tf.keras.callbacks.ModelCheckpoint(
+            filepath=os.path.join(network_checkpoints_path, 'model_{epoch}.h5'),
+            monitor='val_loss', verbose=1, save_best_only=False, save_weights_only=False,
+            mode='auto', save_freq='epoch')
