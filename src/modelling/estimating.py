d820cd8b57cebf31b66389c20489c81c1eee1e42
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-29 15:05:12 +0100

    Add src/modelling/estimating.py

---
+++ src/modelling/estimating.py
@@ -0,0 +1,18 @@
+"""
+Estimating a model's parameters
+"""
+
+
+class Estimating:
+    """
+    Class Estimating
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def network(self):
+        """
+        
+        :return;
+        """
