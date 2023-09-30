85e2b16c4484ccb11b1f8daf43bbf013342bb6a5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-30 18:31:39 +0100

    Update src/modelling/evaluating.py: In Progress

--- src/modelling/evaluating.py
+++ src/modelling/evaluating.py
@@ -1,9 +1,16 @@
 """
 For evaluating various aspects of a model
 """
+import ast
+
+import pandas as pd
+import tensorflow as tf
+
 import src.elements.generators
+import src.elements.metadata
 import src.elements.partitions
 import src.elements.settings
+import src.evaluation.predictions
 
 
 class Evaluating:
@@ -11,22 +18,25 @@ class Evaluating:
     Class Evaluating
     """
 
-    def __init__(self, settings: src.elements.settings.Settings,
+    def __init__(self, metadata: src.elements.metadata.Metadata, 
+                 settings: src.elements.settings.Settings,
                  generators: src.elements.generators.Generators,
                  partitions: src.elements.partitions.Partitions) -> None:
         """
 
+        :param metadata:
         :param settings:
         :param generators:
         :param partitions:
         :return:        
         """
 
+        self.__metadata = metadata
         self.__settings = settings
         self.__generators = generators
         self.__partitions = partitions
 
-    def exc(self):
+    def exc(self, model: tf.keras.Sequential, identifier: str):
         """
         
         :return:
@@ -35,3 +45,12 @@ class Evaluating:
         print(self.__settings._fields)
         print(self.__generators._fields)
         print(self.__partitions._fields)
+
+        predictions = src.evaluation.predictions.Predictions(
+            identifier=identifier, metadata=self.__metadata, settings=self.__settings)
+
+        for field in self.__partitions._fields:
+
+            partition_: pd.DataFrame = ast.literal_eval(f'self.__partitions.{field}')
+            generator_: tf.data.Dataset = ast.literal_eval(f'self.__generators.{field}')
+            predictions.exc(model=model, partition_=partition_, generator_=generator_, name=field)
