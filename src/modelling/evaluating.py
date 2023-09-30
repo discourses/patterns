825221bf20a98072469a1ab990c5d5b6c0cb7a55
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-30 20:28:27 +0100

    Update src/modelling/evaluating.py

--- src/modelling/evaluating.py
+++ src/modelling/evaluating.py
@@ -2,7 +2,9 @@
 For evaluating various aspects of a model
 """
 import ast
+import os
 
+import numpy as np
 import pandas as pd
 import tensorflow as tf
 
@@ -10,7 +12,12 @@ import src.elements.generators
 import src.elements.metadata
 import src.elements.partitions
 import src.elements.settings
+
+import src.evaluation.frequencies
+import src.evaluation.losses
 import src.evaluation.predictions
+
+import src.functions.streams
 
 
 class Evaluating:
@@ -18,7 +25,7 @@ class Evaluating:
     Class Evaluating
     """
 
-    def __init__(self, metadata: src.elements.metadata.Metadata, 
+    def __init__(self, metadata: src.elements.metadata.Metadata,
                  settings: src.elements.settings.Settings,
                  generators: src.elements.generators.Generators,
                  partitions: src.elements.partitions.Partitions) -> None:
@@ -36,21 +43,61 @@ class Evaluating:
         self.__generators = generators
         self.__partitions = partitions
 
-    def exc(self, model: tf.keras.Sequential, identifier: str):
+    def __losses(self, history, pathway: str):
         """
         
+        :param history:
+        :param pathway:
         :return:
         """
 
-        print(self.__settings._fields)
-        print(self.__generators._fields)
-        print(self.__partitions._fields)
+        src.evaluation.losses.Losses().exc(
+            history=history, path=pathway)
 
+    def __predictions(self, model: tf.keras.Sequential, pathway: str):
+        """
+        
+        :param model:
+        :param pathway:
+        :return:
+        """
+
+        # The thresholds
+        thresholds = np.arange(
+            start=self.__settings.threshold_min, stop=self.__settings.threshold_max, step=self.__settings.threshold_step)
+
+        # Predictions
         predictions = src.evaluation.predictions.Predictions(
-            identifier=identifier, metadata=self.__metadata, settings=self.__settings)
+            pathway=pathway, metadata=self.__metadata, settings=self.__settings)
 
+        streams = src.functions.streams.Streams()
         for field in self.__partitions._fields:
 
+            # Predictions
             partition_: pd.DataFrame = ast.literal_eval(f'self.__partitions.{field}')
             generator_: tf.data.Dataset = ast.literal_eval(f'self.__generators.{field}')
-            predictions.exc(model=model, partition_=partition_, generator_=generator_, name=field)
+            plausibilities = predictions.exc(model=model, partition_=partition_, generator_=generator_, name=field)
+
+            # Truth
+            truth = partition_[self.__metadata.labels].values
+
+            # Error matrix frequencies
+            data = src.evaluation.frequencies.Frequencies(
+                thresholds=thresholds, plausibilities=plausibilities, truth=truth, classes=self.__metadata.labels)
+
+            streams.write(blob=data, path=os.path.join(pathway, f'frequencies_{field}.csv'))
+
+    def exc(self, history, model: tf.keras.Sequential, pathway: str):
+        """
+        
+        :param history:
+        :param model:
+        :param pathway:
+        :return:
+        """
+
+        assert np.setdiff1d(self.__generators._fields,
+                            self.__partitions._fields).shape[0] == 0, 'There might be a set-up error w.r.t. modelling data'
+
+        self.__losses(history=history, pathway=pathway)
+        self.__predictions(model=model, pathway=pathway)
