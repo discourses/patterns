03fe9e8ffdb958caf6a749bb41fc72d85bf9332d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-30 13:24:53 +0100

    Update src/modelling/steps.py

--- src/modelling/steps.py
+++ src/modelling/steps.py
@@ -1,9 +1,13 @@
 """
 The modelling steps for interface.py
 """
+
+import src.modelling.estimating
 import src.modelling.hyperparameters
 import src.modelling.architecture
+
 import src.elements.attributes
+import src.elements.settings
 import src.elements.generators
 import src.elements.metadata
 import src.elements.partitions
@@ -13,14 +17,20 @@ class Steps:
     Class Steps
     """
 
-    def __init__(self, attributes: src.elements.attributes.Attributes, metadata: src.elements.metadata.Metadata) -> None:
+    def __init__(self, attributes: src.elements.attributes.Attributes, metadata: src.elements.metadata.Metadata, 
+                 settings: src.elements.settings.Settings) -> None:
         """
         Constructor
         """
 
-        self.__hyperparameters = src.modelling.hyperparameters.Hyperparameters().exc()
-        self.__architecture = src.modelling.architecture.Architecture(attributes=attributes)
         self.__metadata = metadata
+        self.__settings = settings
+
+        # The deep learning architecture of interest
+        self.__architecture = src.modelling.architecture.Architecture(attributes=attributes)
+
+        # A list of hyperparameter collections for training the deep learning model
+        self.__hyperparameters = src.modelling.hyperparameters.Hyperparameters().exc()
 
     def exc(self, generators: src.elements.generators.Generators, partitions: src.elements.partitions.Partitions):
         """
@@ -28,14 +38,18 @@ class Steps:
         :return:
         """
 
+        estimating = src.modelling.estimating.Estimating(
+            settings=self.__settings, generators=generators, partitions=partitions)
+
         index: int = 0
 
         for hpc in self.__hyperparameters:
 
             index = index + 1
-            str(index).zfill(4)
+            identifier = str(index).zfill(4)
 
             model = self.__architecture.exc(hpc=hpc, labels=self.__metadata.labels)
-            print(type(model))
-            print(generators._fields)
-            print(partitions._fields)
+
+            history = estimating.exc(model=model, identifier=identifier)
+
+            print(history)
